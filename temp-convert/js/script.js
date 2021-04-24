af4befccd6a11bcc9a427528902ab5c9ddba1c50
leifgio.villanueva@gsfe.tupcavite.edu.ph <leifgio.villanueva@gsfe.tupcavite.edu.ph>
2021-04-24 19:36:24 +0800

    add comment to script.js

--- temp-convert/js/script.js
+++ temp-convert/js/script.js
@@ -1,18 +1,22 @@
+/* set id to variable to increase readability of code */
 var cel = document.getElementById("celsius");
 var far = document.getElementById("farenheit");
 
+/* convert celsius to farenheit */
 function celToFar() {
   var celsius = document.getElementById("num").value;
   var farenheit = Math.round((celsius * 9) / 5 + 32);
   alert("the converted value of celsius to farenheit is " + farenheit);
 }
 
+/* convert farenheit to celsius */
 function farToCel() {
   var farenheit = document.getElementById("num").value;
   var celsius = Math.round(((farenheit - 32) * 5) / 9);
   alert("the converted value of farenheit to celsius is " + celsius);
 }
 
+/* choose what function to use */
 function convert() {
   if (cel.checked) {
     celToFar();
@@ -21,11 +25,7 @@ function convert() {
   }
 }
 
-/* var x = document.getElementById("convert");
-x.addEventListener("click", function () {
-  celToFar();
-}); */
-
+/* add event listener to button */
 var x = document.getElementById("convert");
 x.addEventListener("click", function () {
   convert();
