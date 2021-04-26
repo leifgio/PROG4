d678bb7ac55c2d14b690e65f390c4ae391339b9f
leifgio.villanueva@gsfe.tupcavite.edu.ph <leifgio.villanueva@gsfe.tupcavite.edu.ph>
2021-04-26 20:21:01 +0800

    change temp-convert

--- temp-convert/js/script.js
+++ temp-convert/js/script.js
@@ -2,18 +2,20 @@
 var cel = document.getElementById("celsius");
 var far = document.getElementById("farenheit");
 
-/* convert celsius to farenheit */
+/* convert celsius to farenheit and limit to two decimal places*/
 function celToFar() {
   var celsius = document.getElementById("num").value;
-  var farenheit = Math.round((celsius * 9) / 5 + 32);
-  alert("the converted value of celsius to farenheit is " + farenheit);
+  var farenheit = ((celsius * 9) / 5 + 32);
+  farenheit = farenheit.toFixed(2); /* method to limit decimal places*/
+  alert("the converted value of celsius to farenheit is " + farenheit + "℉");
 }
 
 /* convert farenheit to celsius */
 function farToCel() {
   var farenheit = document.getElementById("num").value;
-  var celsius = Math.round(((farenheit - 32) * 5) / 9);
-  alert("the converted value of farenheit to celsius is " + celsius);
+  var celsius = (((farenheit - 32) * 5) / 9);
+  celsius = celsius.toFixed(2);
+  alert("the converted value of farenheit to celsius is " + celsius+ "℃");
 }
 
 /* choose what function to use */
